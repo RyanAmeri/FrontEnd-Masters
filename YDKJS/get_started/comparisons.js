ea41960f618a4d31fa230c422eb35b6a019509e7
Ryan Ameri <ryan@internettradie.com.au>
2020-07-10 17:21:06 +1000

    implemented comparison by myself using Date objects. I'm 99% this is not what Kyle wanted me to do as I have avoided using any comparisons but when we are talking about time using Date objects makes more sense to me than anything else

--- YDKJS/get_started/comparisons.js
+++ YDKJS/get_started/comparisons.js
@@ -2,13 +2,22 @@ const dayStart = "07:30";
 const dayEnd = "17:45";
 
 function scheduleMeeting(startTime,durationMinutes) {
-    // ..TODO..
+    const [dayStartHour,dayStartMinute] = dayStart.split(":");
+    const [meetingStartHour,meetingStartMinute] = startTime.split(":");
+    const [dayEndHour,dayEndMinute] = dayEnd.split(":");
+    const dayStartDate = new Date(Date.UTC(2000,0,1,dayStartHour,dayStartMinute,0));
+    const dayEndDate = new Date(Date.UTC(2000,0,1,dayEndHour,dayEndMinute,0));
+    const meetingStartDate = new Date(Date.UTC(2000,0,1,meetingStartHour,meetingStartMinute,0));
+    const meetingEndDate = new Date(Date.UTC(2000,0,1,meetingStartHour,meetingStartMinute,0));
+    meetingEndDate.setMinutes(meetingEndDate.getMinutes() + Number(durationMinutes));
+    return meetingStartDate.getTime() >= dayStartDate.getTime() && meetingEndDate.getTime() <= dayEndDate.getTime() ? true : false;
 }
 
-scheduleMeeting("7:00",15);     // false
-scheduleMeeting("07:15",30);    // false
-scheduleMeeting("7:30",30);     // true
-scheduleMeeting("11:30",60);    // true
-scheduleMeeting("17:00",45);    // true
-scheduleMeeting("17:30",30);    // false
-scheduleMeeting("18:00",15);    // false
+
+console.log(scheduleMeeting("7:00",15));     // false
+console.log(scheduleMeeting("07:15",30));    // false
+console.log(scheduleMeeting("7:30",30));     // true
+console.log(scheduleMeeting("11:30",60));    // true
+console.log(scheduleMeeting("17:00",45));    // true
+console.log(scheduleMeeting("17:30",30));    // false
+console.log(scheduleMeeting("18:00",15));    // false
