a4b8b2539f550e3ee79f2aa0395bc2bc9edfc2d2
Ryan Ameri <ryan@internettradie.com.au>
2020-07-10 16:23:04 +1000

    initial commit of pracice.js exercise for YDKJS getting started

---
+++ YDKJS/get_started/comparisons.js
@@ -0,0 +1,14 @@
+const dayStart = "07:30";
+const dayEnd = "17:45";
+
+function scheduleMeeting(startTime,durationMinutes) {
+    // ..TODO..
+}
+
+scheduleMeeting("7:00",15);     // false
+scheduleMeeting("07:15",30);    // false
+scheduleMeeting("7:30",30);     // true
+scheduleMeeting("11:30",60);    // true
+scheduleMeeting("17:00",45);    // true
+scheduleMeeting("17:30",30);    // false
+scheduleMeeting("18:00",15);    // false
