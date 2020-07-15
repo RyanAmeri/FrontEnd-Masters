db25f286adfebb23cf89ebc295780d43234e982c
Ryan Ameri <ryan@internettradie.com.au>
2020-07-15 18:12:36 +1000

    implemented three pillars exercise using class syntax

---
+++ YDKJS/get_started/exercises/three-pillars/ex.js
@@ -0,0 +1,45 @@
+class Bookshelf {
+  constructor() {
+    this.favoriteBooks = [];
+  }
+  addFavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+  printFavoriteBooks() {
+    console.log(`Favorite Books: ${this.favoriteBooks.length}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(bookName);
+    }
+  }
+}
+
+function loadBooks(bookShelf) {
+  fakeAjax(BOOK_API, (bookArray) => {
+    for (let book of bookArray) {
+      bookShelf.addFavoriteBook(book);
+    }
+    bookShelf.printFavoriteBooks();
+  });
+}
+
+const BOOK_API = "https://some.url/api";
+
+// ***********************
+
+// NOTE: don't modify this function at all
+function fakeAjax(url, cb) {
+  setTimeout(function fakeLoadingDelay() {
+    cb([
+      "A Song of Ice and Fire",
+      "The Great Gatsby",
+      "Crime & Punishment",
+      "Great Expectations",
+      "You Don't Know JS",
+    ]);
+  }, 500);
+}
+
+const bsInstance = new Bookshelf();
+loadBooks(bsInstance);
