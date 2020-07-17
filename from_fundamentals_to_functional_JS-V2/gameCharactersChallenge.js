d3af77763c3d016fa575507bd8c859d3f9228641
Ryan Ameri <ryan@internettradie.com.au>
2020-07-17 14:20:41 +1000

    Created JavaScript: From Fundamentals to Functional JS, v2 directory and solved the first game character challenge

---
+++ from_fundamentals_to_functional_JS-V2/gameCharactersChallenge.js
@@ -0,0 +1,4 @@
+const clue = Object.create(null);
+clue.characters = ["Mr. White", "Mr. Pink", "Mr. Brown"];
+clue.rooms = ["kitchen", "parlor", "dining"];
+clue["weapons"] = ["knife", "sword", "candle stick"];
