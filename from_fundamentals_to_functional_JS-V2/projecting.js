d87d605571d71297b1054d7665dd1194393b6b33
Ryan Ameri <ryan@internettradie.com.au>
2020-07-20 14:07:33 +1000

    implemented projecting exercise

---
+++ from_fundamentals_to_functional_JS-V2/projecting.js
@@ -0,0 +1,78 @@
+import { _ } from "./_.js";
+const videoData = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Mrs. White",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Reverend Green",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Rusty",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+];
+const suspectObjs = _.filter(videoData, (item) => item.present === true);
+const suspectList = _.map(suspectObjs, (suspect) => suspect.name);
+console.log(suspectList);
