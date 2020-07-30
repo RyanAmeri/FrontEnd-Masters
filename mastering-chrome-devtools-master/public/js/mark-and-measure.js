53d9fca4a870566462ecb0b18bc97319140f32b8
Ryan Ameri <ryan@internettradie.com.au>
2020-07-30 13:54:40 +1000

    implemented mark and measure exetcise using Performance API

--- mastering-chrome-devtools-master/public/js/mark-and-measure.js
+++ mastering-chrome-devtools-master/public/js/mark-and-measure.js
@@ -1,14 +1,18 @@
 window.addEventListener("load", thingToMeasure);
 
 function thingToMeasure() {
-  const start = new Date().getTime();
+  //const start = new Date().getTime();
+  performance.mark("start");
 
   fetch("/api")
     .then((result) => result.json())
     .then((json) => {
       json.images.forEach((image) => console.log(image.name));
-      const end = new Date().getTime();
-      const time = end - start;
-      console.log(time);
+      //  const end = new Date().getTime();
+      //  const time = end - start;
+      //  console.log(time);
+      performance.mark("end");
+      performance.measure("Our Measurement", "start", "end");
+      console.log(performance.getEntriesByType("measure"));
     });
 }
