66e8d468f8a6ae0eff7323de6259717b7dcb8170
Ryan Ameri <ryan@internettradie.com.au>
2020-07-30 13:42:53 +1000

    implemented mark and measure exercise using date objects

--- mastering-chrome-devtools-master/public/js/mark-and-measure.js
+++ mastering-chrome-devtools-master/public/js/mark-and-measure.js
@@ -1,9 +1,14 @@
 window.addEventListener("load", thingToMeasure);
 
 function thingToMeasure() {
+  const start = new Date().getTime();
+
   fetch("/api")
-    .then(result => result.json())
-    .then(json => {
-      json.images.forEach(image => console.log(image.name));
+    .then((result) => result.json())
+    .then((json) => {
+      json.images.forEach((image) => console.log(image.name));
+      const end = new Date().getTime();
+      const time = end - start;
+      console.log(time);
     });
 }
