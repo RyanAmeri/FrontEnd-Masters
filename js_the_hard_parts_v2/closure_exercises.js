4306a7afc011325883c312629d95a778100dd9c2
Ryan Ameri <ryan@internettradie.com.au>
2020-08-03 19:46:05 +1000

    uncommented one previously missed test

--- js_the_hard_parts_v2/closure_exercises.js
+++ js_the_hard_parts_v2/closure_exercises.js
@@ -385,13 +385,13 @@ function average() {
 }
 
 // /*** Uncomment these to check your work! ***/
-//const avgSoFar = average();
-//console.log(avgSoFar()); // => should log 0
-//console.log(avgSoFar(4)); // => should log 4
-//console.log(avgSoFar(8)); // => should log 6
-//console.log(avgSoFar()); // => should log 6
-//console.log(avgSoFar(12)); // => should log 8
-//console.log(avgSoFar()); // => should log 8
+const avgSoFar = average();
+console.log(avgSoFar()); // => should log 0
+console.log(avgSoFar(4)); // => should log 4
+console.log(avgSoFar(8)); // => should log 6
+console.log(avgSoFar()); // => should log 6
+console.log(avgSoFar(12)); // => should log 8
+console.log(avgSoFar()); // => should log 8
 
 // CHALLENGE 17
 /* 
