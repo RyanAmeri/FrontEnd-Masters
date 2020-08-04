73627a652911bd12d64926f57d3ca33b86bc1df3
Ryan Ameri <ryan@internettradie.com.au>
2020-08-05 07:41:25 +1000

    implmented promise exercise

--- js_the_hard_parts_v2/async_exercises.js
+++ js_the_hard_parts_v2/async_exercises.js
@@ -113,21 +113,27 @@ function delayCounter(target, wait) {
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-const countLogger = delayCounter(3, 1000);
-countLogger();
+//const countLogger = delayCounter(3, 1000);
+//countLogger();
 //After 1 second, log 1
 //After 2 seconds, log 2
 //After 3 seconds, log 3
 
-/* CHALLENGE 8 */
+/* CHALLENGE 8 
+Write a function, promised, that takes in a value. This function will return a promise that will resolve after 2 seconds.
+
+Hint: take a look at the Promise object docs on MDN.
+*/
 
 function promised(val) {
-  // ADD CODE HERE
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(val), 2000);
+  });
 }
 
 // UNCOMMENT THESE TO TEST YOUR WORK!
-// const createPromise = promised('wait for it...');
-// createPromise.then((val) => console.log(val));
+const createPromise = promised("wait for it...");
+createPromise.then((val) => console.log(val));
 // will log "wait for it..." to the console after 2 seconds
 
 /* CHALLENGE 9 */
